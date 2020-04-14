8294cd93597102b08e4c1f4731b36b6b60dc75f6
Antipenko Gleb <antipenko.g.d@gmail.com>
2020-04-14 14:46:52 +0300

    feat: add solution for task3

--- src/main/kotlin/subtask3/Abbreviation.kt
+++ src/main/kotlin/subtask3/Abbreviation.kt
@@ -4,6 +4,13 @@ class Abbreviation {
 
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        val result = b.toMutableList()
+        var step = 0
+        while (step < a.length && result.isNotEmpty()) {
+            if (a.toUpperCase()[step] == result[0])
+                result.removeAt(0)
+            step++
+        }
+        return if (result.isEmpty()) "YES" else "NO"
     }
 }
