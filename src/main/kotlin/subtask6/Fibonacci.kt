408d62a16ab01d9804024bffc47ee6e30468864f
Antipenko Gleb <antipenko.g.d@gmail.com>
2020-04-14 17:07:58 +0300

    feat: add solution for task6

--- src/main/kotlin/subtask6/Fibonacci.kt
+++ src/main/kotlin/subtask6/Fibonacci.kt
@@ -4,6 +4,19 @@ class Fibonacci {
 
     // TODO: Complete the following function
     fun productFibonacciSequenceFor(n: Int): IntArray {
-        throw NotImplementedError("Not implemented")
+        var prev = 0
+        var current = 1
+
+        var step = 1
+        while (step <= n) {
+            val newCurrent = prev + current
+            prev = current
+            current = newCurrent
+            if (prev * current >= n) {
+                return intArrayOf(prev, current, if (prev * current == n) 1 else 0)
+            }
+            step++
+        }
+        return intArrayOf()
     }
 }
